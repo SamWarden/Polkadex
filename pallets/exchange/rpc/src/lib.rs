76eb2287de7905c805abc230af1601e320ced811
Ubuntu <ubuntu@ip-172-31-9-58.eu-central-1.compute.internal>
2021-06-24 21:58:20 +0000

    Add exchange

---
+++ pallets/exchange/rpc/src/lib.rs
@@ -0,0 +1,54 @@
+//! RPC interface for the transaction payment module.
+
+use jsonrpc_core::{Error as RpcError, ErrorCode, Result};
+use jsonrpc_derive::rpc;
+use sp_api::ProvideRuntimeApi;
+use sp_blockchain::HeaderBackend;
+use sp_runtime::{generic::BlockId, traits::Block as BlockT};
+use std::sync::Arc;
+use exchange_runtime_api::ExchangeApi as ExchangeRuntimeApi;
+
+#[rpc]
+pub trait ExchangeApi<BlockHash> {
+	#[rpc(name = "submit_order")]
+	fn submit_order(&self, at: Option<BlockHash>) -> Result<u32>;
+}
+
+/// A struct that implements the `Exchange`.
+pub struct Exchange<C, M> {
+	client: Arc<C>,
+	_marker: std::marker::PhantomData<M>,
+}
+
+impl<C, M> Exchange<C, M> {
+	/// Create new `Exchange` instance with the given reference to the client.
+	pub fn new(client: Arc<C>) -> Self {
+		Self {
+			client,
+			_marker: Default::default(),
+		}
+	}
+}
+
+impl<C, Block> ExchangeApi<<Block as BlockT>::Hash> for Exchange<C, Block>
+where
+	Block: BlockT,
+	C: Send + Sync + 'static,
+	C: ProvideRuntimeApi<Block>,
+	C: HeaderBackend<Block>,
+	C::Api: ExchangeRuntimeApi<Block>,
+{
+	fn submit_order(&self, at: Option<<Block as BlockT>::Hash>) -> Result<u32> {
+		let api = self.client.runtime_api();
+		let at = BlockId::hash(at.unwrap_or_else(||
+			// If the block hash is not supplied assume the best block.
+			self.client.info().best_hash));
+
+		let runtime_api_result = api.submit_order(&at);
+		runtime_api_result.map_err(|e| RpcError {
+			code: ErrorCode::ServerError(9876), // No real reason for this value
+			message: "Something wrong".into(),
+			data: Some(format!("{:?}", e).into()),
+		})
+	}
+}
