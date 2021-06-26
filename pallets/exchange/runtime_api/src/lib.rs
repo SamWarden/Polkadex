4b0ae1c524d60b4077cc88997fbdb62e34edadd2
Ubuntu <ubuntu@ip-172-31-9-58.eu-central-1.compute.internal>
2021-06-26 20:52:59 +0000

    Fix a type that a method returns in the runtime-api/lib.rs

--- pallets/exchange/runtime_api/src/lib.rs
+++ pallets/exchange/runtime_api/src/lib.rs
@@ -2,13 +2,13 @@
 #![allow(clippy::too_many_arguments)]
 #![allow(clippy::unnecessary_mut_passed)]
 
-use frame_support::{dispatch::DispatchResultWithPostInfo}; //, dispatch::DispatchResult};
+//use frame_support::{dispatch::DispatchResultWithPostInfo}; //, dispatch::DispatchResult};
 
 // Here we declare the runtime API. It is implemented it the `impl` block in
 // runtime amalgamator file (the `runtime/src/lib.rs`)
 sp_api::decl_runtime_apis! {
 	pub trait ExchangeApi {
-		fn submit_order() -> DispatchResultWithPostInfo;
+		fn submit_order() -> u32;
 		//fn exchange() -> u32;
 	}
 }
